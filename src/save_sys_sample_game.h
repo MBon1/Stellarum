967c1a0e95cce5725bc10f342b97f70c341c4d5b
Matthew Bonnecaze <bonnecaze.matthew@gmail.com>
2022-12-08 00:02:20 -0500

    Fixed comments for sample game header

--- src/save_sys_sample_game.h
+++ src/save_sys_sample_game.h
@@ -1,7 +1,7 @@
 #pragma once
 
-// Frogger Game
-// A simple Frogger-like game.
+// Save System Sample Game
+// A simple test game to demo the save system.
 
 typedef struct save_sys_sample_game_t save_sys_sample_game_t;
 
@@ -10,11 +10,11 @@ typedef struct heap_t heap_t;
 typedef struct render_t render_t;
 typedef struct wm_window_t wm_window_t;
 
-// Create an instance of Frogger game.
+// Create an instance of Save System Sample Game.
 save_sys_sample_game_t* save_sys_sample_game_create(heap_t* heap, fs_t* fs, wm_window_t* window, render_t* render);
 
-// Destroy an instance of Frogger game.
+// Destroy an instance of Save System Sample Game.
 void save_sys_sample_game_destroy(save_sys_sample_game_t* game);
 
-// Per-frame update for our Frogger game.
+// Per-frame update for our Save System Sample Game.
 void save_sys_sample_game_update(save_sys_sample_game_t* game);
